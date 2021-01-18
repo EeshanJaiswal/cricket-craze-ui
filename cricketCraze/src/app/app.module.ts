f2ffc589ba4be9e91ed50e3dba3dfe1b222c9655
eeshanj <eeshanj@gmail.com>
2021-01-17 22:10:28 -0800

    bootstrap refs added , layout component

--- cricketCraze/src/app/app.module.ts
+++ cricketCraze/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import {BlockModuleModule} from "./block-module/block-module.module";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
+    BlockModuleModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- cricketCraze/src/app/block-module/block-module.module.ts
+++ cricketCraze/src/app/block-module/block-module.module.ts
@@ -4,13 +4,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LayoutOneComponent } from './layout-one/layout-one.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent, LoginComponent, RegisterComponent],
+  declarations: [NavbarComponent, FooterComponent, LoginComponent, RegisterComponent, LayoutOneComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [NavbarComponent, FooterComponent, LoginComponent, RegisterComponent, LayoutOneComponent]
+
 })
 export class BlockModuleModule { }
